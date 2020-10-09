751241e4ea501e5dae9416e4752c6e080eb4fce4
abdulfataiaka <abdulfataiaka@gmail.com>
2020-10-09 18:59:11 +0100

    update scripts

--- packages/athapt-base/src/config/index.js
+++ packages/athapt-base/src/config/index.js
@@ -1,6 +1,6 @@
 class Config {
-  port = 8000;
-  host = '127.0.0.1';
+  port = process.env.ATHAPT_PORT || 8000;
+  host = process.env.ATHAPT_HOST || '127.0.0.1';
 }
 
 export default Config;
--- sandbox/index.js
+++ sandbox/index.js
@@ -1,4 +1,4 @@
-const { default: Athapt } = require('@athapt/base');
+import Athapt from '@athapt/base';
 
 const server = new Athapt();
 
@@ -6,14 +6,13 @@ const server = new Athapt();
 //  Register parsers for the server
 //========================================
 
-server.parsers.register('rest');  // [!] install @athapt/rest
+server.parsers.register('rest');
 
 //========================================
 //  Configure server behaviour
 //========================================
 
-server.config.port = 8000;
-server.config.host = '127.0.0.1';
+server.config.port = 7000;
 
 //========================================
 //  Register init codes
